5d31b3060138bc8bf89b9ebef65fa407f7548e3a
AstridHG <astrid1404hg@gmail.com>
2020-08-26 12:23:15 -0600

    Actualizacion de frontend

--- Frontend-SAOP/src/app/app-routing.module.ts
+++ Frontend-SAOP/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {VisualizacionComponent} from "./components/visualizacion/visualizacion.component";
+import {HomeComponent} from "./components/home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path: 'visualizacion-cursos',
+    component: VisualizacionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
