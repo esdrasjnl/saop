760834304b765f2655cc09767754691fe0e3ecc1
esdrasjnl <esdrasnl@gmail.com>
2020-08-28 22:23:11 -0600

    Metodo eliminar usuario

--- backend/controller/usuario.controller.js
+++ backend/controller/usuario.controller.js
@@ -43,6 +43,7 @@ usuarioCtrl.postUsuario=async function(req,res,next){
         res.send('Usuario Creado');
     });
 }
+
 usuarioCtrl.getUserForcarnet=async function(req,res,next){
     const { carnet } = req.params;
     const sql = `select * from usuario where carnet = ${carnet}`;
@@ -54,5 +55,15 @@ usuarioCtrl.getUserForcarnet=async function(req,res,next){
             res.send('No hay resultados');
         }
     });
+}   
+
+usuarioCtrl.deleteUser = async function(req,res,next){
+    const { carnet } = req.params;
+    const sql = `delete from usuario where carnet = ${carnet}`;
+    mysqldb.connection.query(sql, error => {
+        if(error) throw error;
+        res.send('Usuario Eliminado');
+    });
 }
+
 module.exports=usuarioCtrl;
