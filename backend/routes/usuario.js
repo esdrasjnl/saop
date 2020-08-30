e2f2310f38532ef6e2072e824c79c5847584dc35
esdrasjnl <esdrasnl@gmail.com>
2020-08-29 18:28:43 -0600

    Se borraron metodos innecesarios de la conexión a la base de datos

--- backend/routes/usuario.js
+++ backend/routes/usuario.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const bd = require('../database/db');
+const bd = require('../database');
 
 router.get('/obtener', (req, res) => {
     const sql = 'select * from usuario';
