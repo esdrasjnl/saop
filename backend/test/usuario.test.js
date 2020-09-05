bb8434b31139689df632390d55625271107eabdc
yovany <syovany@gmail.com>
2020-09-04 21:22:20 -0600

    prueba unitaria de login

---
+++ backend/test/usuario.test.js
@@ -0,0 +1,42 @@
+const assert=require("assert");
+const request=require("supertest");
+const usuarios=require("../routes/usuario.router");
+var express = require('express');
+const { response } = require("express");
+var app = express();
+
+describe("Express usuarios",async ()=>{
+  it("Handles GET request /id",done=>{
+     request(app.use(usuarios))
+     .get('/2023')
+     .end((err,response)=>{
+         //assert(response.body.usuarios==="OK");
+         assert(typeof usuarios,'object');
+         done();
+     })
+  });
+  it("Handles2 GET request /id",done=>{
+      request(app.use(usuarios))
+      .get('/2023')
+      .end((err,response)=>{
+        assert(typeof usuarios.getUsuarioId,'function');
+        done();
+      });
+  });
+  it("TestLogin POST request /inicioSesion",done=>{
+    request(app.use(usuarios))
+    .post('{carnet:2023,clave:123}')
+    .end((err,response)=>{
+      assert(typeof usuarios,'object');
+      done();
+    });
+  });
+  it("TestLoginFuction POST request /inicioSesion", done=>{
+    request(app.use(usuarios))
+    .post("{carnet: 2023,clave:123}")
+    .end((err,response)=>{
+       assert(typeof usuarios.inicioSesion,'functoion');
+       done();
+    });
+  });
+});
