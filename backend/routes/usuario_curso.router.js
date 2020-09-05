0f2d1925fd4e3e7a1b9a22c80fca60bf9f8154a3
yovany <syovany@gmail.com>
2020-09-04 22:31:55 -0600

    prueba unitaria para obtener curso en detalle para usuario

--- backend/routes/usuario_curso.router.js
+++ backend/routes/usuario_curso.router.js
@@ -3,6 +3,6 @@ const router=express.Router();
 
 const userCurso=require('../controller/usuario_curso.controller');
 
-router.get('/:id',userCurso.getUsuarioCurso);
+router.get('/id',userCurso.getUsuarioCurso);
 router.post('/detalleCurso',userCurso.getDetalleCurso);
 module.exports=router;
---
+++ backend/test/usuario_curso.test.js
@@ -0,0 +1,17 @@
+const assert=require("assert");
+const request=require("supertest");
+const ususuario_curso=require("../routes/usuario_curso.router");
+var express = require('express');
+const { response } = require("express");
+var app = express();
+
+describe("Express usuario_curso",async ()=>{
+    it("TetsCurso_usuario GET request /id", done=>{
+      request(app.use(ususuario_curso))
+      .get("/091")
+      .end((err,response)=>{
+        assert(typeof ususuario_curso,'object');
+        done();
+      });
+    });
+})
