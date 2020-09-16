918d00a2582cb1c544253859ae3a4b7efd9fa10e
Edwar Zacarias <cuentaroja11@gmail.com>
2020-09-15 21:54:24 -0600

    Implementacion pruevas unitarias app.component

--- Frontend-SAOP/src/app/app.component.spec.ts
+++ Frontend-SAOP/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'Frontend-SAOP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Frontend-SAOP');
+    expect(app.title).toEqual('Se esperaba Frontend-SAOP');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Frontend-SAOP app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Se esperaba Frontend-SAOP');
   });
 });
