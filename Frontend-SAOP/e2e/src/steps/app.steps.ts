537ac03d0899e082fd3cc9c818c41c83cc8fe98d
AstridHG <astrid1404hg@gmail.com>
2020-09-17 19:38:26 -0600

    Se realizaron configuraciones para la realizacion de bdd

---
+++ Frontend-SAOP/e2e/src/steps/app.steps.ts
@@ -0,0 +1,20 @@
+import { Before, Given, Then, When } from 'cucumber';
+import { expect } from 'chai';
+
+import { AppPage } from '../pages/app.po';
+
+let page: AppPage;
+
+Before(() => {
+  page = new AppPage();
+});
+
+Given(/^I am on the home page$/, {timeout:60 * 5000}, async () => {
+  await page.navigateTo();
+});
+
+When(/^I do nothing$/, () => {});
+
+Then(/^I should see the title$/, async () => {
+  expect(await page.getTitleText()).to.equal('home works!');
+});
