cba6d0236a5d3fd6f7ea5183744293edbec0e416
Esdras <esdrasjnl@gmail.com>
2020-10-20 12:21:43 -0600

    Se agregaron validaciones para asignación de curso

--- backend/controller/usuario_curso.controller.js
+++ backend/controller/usuario_curso.controller.js
@@ -1,19 +1,19 @@
-const userCourseCtrl={};
-const mysqldb=require('../database');
+const userCourseCtrl = {};
+const mysqldb = require('../database');
 
-userCourseCtrl.getUsuarioCurso=async function(req,res,next){
-    const {id}=req.params;
-    const sql=`SELECT usuario.nombre,usuario.carnet,`+
-    `curso.codigo_curso,curso.nombre_curso,curso.creditos,`+
-    `usuario_curso.estado `+
-    `from usuario_curso `+
-    `INNER JOIN usuario ON usuario_curso.ref_carnet=usuario.carnet `+
-    `INNER JOIN curso ON  usuario_curso.ref_codigo_curso=codigo_curso `+
-    `WHERE usuario.carnet = '${id}'`;
+userCourseCtrl.getUsuarioCurso = async function (req, res, next) {
+    const { id } = req.params;
+    const sql = `SELECT usuario.nombre,usuario.carnet,` +
+        `curso.codigo_curso,curso.nombre_curso,curso.creditos,` +
+        `usuario_curso.estado ` +
+        `from usuario_curso ` +
+        `INNER JOIN usuario ON usuario_curso.ref_carnet=usuario.carnet ` +
+        `INNER JOIN curso ON  usuario_curso.ref_codigo_curso=codigo_curso ` +
+        `WHERE usuario.carnet = '${id}'`;
     console.log(sql);
-        mysqldb.connection.query(sql, (error, results) => {
-        if(error) throw error;
-        if(results.length >= 0){
+    mysqldb.connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length >= 0) {
             return res.json(results);
         } else {
             return res.send('No hay resultados');
@@ -21,43 +21,57 @@ userCourseCtrl.getUsuarioCurso=async function(req,res,next){
     });
 }
 
-userCourseCtrl.agregaUsuarioCurso = async function(req,res,next){
-    const sql = 'insert into usuario_curso set?';
+userCourseCtrl.agregaUsuarioCurso = async function (req, res, next) {
+    let { ref_carnet, ref_codigo_curso, estado, str_comentario } = req.body;
+    const validacion = `select count(*) as retorno from usuario_curso where ref_carnet = ${ref_carnet} and ref_codigo_curso = ${ref_codigo_curso} and estado = 0`;
 
-    const UsuarioObj = {
-        ref_carnet: req.body.ref_carnet,
-        ref_codigo_curso: req.body.ref_codigo_curso,
-        estado: req.body.estado,
-        str_comentario: req.body.str_comentario
-    };
+    mysqldb.connection.query(validacion, function (err, results) {
+        if (err) {
+            throw err;
+        }
+        //console.log(results[0]);
+        var existeDato = results[0].retorno;
+        console.log(existeDato);
+        if (parseInt(existeDato) === 0) {
+            const sql = 'insert into usuario_curso set?';
+            const UsuarioObj = {
+                ref_carnet: req.body.ref_carnet,
+                ref_codigo_curso: req.body.ref_codigo_curso,
+                estado: req.body.estado,
+                str_comentario: req.body.str_comentario
+            };
 
-    mysqldb.connection.query(sql, UsuarioObj, error => {
-        if(error) throw error;
-        //res.send('Curso Agregado al Usuario');
-        res.json({"Registro": "true"});
+            mysqldb.connection.query(sql, UsuarioObj, error => {
+                if (error) throw error;
+                //res.send('Curso Agregado al Usuario');
+                res.json({ "Registro": "true" });
+            });
+        } else {
+            res.json({ 'estado': 'Datos Repetidos' });
+        }
     });
 }
 
-userCourseCtrl.getDetalleCurso= async function(req,res,next){
-    const objeto={
-        carnet:req.body.carnet,
-        codigo_curso:req.body.codigo_curso
-        }
-    const sql=`SELECT usuario.nombre,usuario.carnet,`+
-    `curso.nombre_curso,curso.creditos,`+
-    `usuario_curso.estado `+
-    `from usuario_curso `+
-    `INNER JOIN usuario ON usuario_curso.ref_carnet=usuario.carnet `+
-    `INNER JOIN curso ON  usuario_curso.ref_codigo_curso=codigo_curso `+
-    `WHERE usuario.carnet=${req.body.carnet} `+
-    `and curso.codigo_curso=${req.body.codigo_curso}`;
+userCourseCtrl.getDetalleCurso = async function (req, res, next) {
+    const objeto = {
+        carnet: req.body.carnet,
+        codigo_curso: req.body.codigo_curso
+    }
+    const sql = `SELECT usuario.nombre,usuario.carnet,` +
+        `curso.nombre_curso,curso.creditos,` +
+        `usuario_curso.estado ` +
+        `from usuario_curso ` +
+        `INNER JOIN usuario ON usuario_curso.ref_carnet=usuario.carnet ` +
+        `INNER JOIN curso ON  usuario_curso.ref_codigo_curso=codigo_curso ` +
+        `WHERE usuario.carnet=${req.body.carnet} ` +
+        `and curso.codigo_curso=${req.body.codigo_curso}`;
 
-        mysqldb.connection.query(sql, [req.body.carnet,req.body.codigo_curso],(error, results) => {         
-            if(results===undefined){
-                return res.json({hola:"fail"});
-            }else{
-                 return res.json(results);
-            }
-        });
+    mysqldb.connection.query(sql, [req.body.carnet, req.body.codigo_curso], (error, results) => {
+        if (results === undefined) {
+            return res.json({ hola: "fail" });
+        } else {
+            return res.json(results);
+        }
+    });
 }
-module.exports=userCourseCtrl;
+module.exports = userCourseCtrl;
