5abbf96265bcc18bcd47247e0f0c1d2e0013a49a
yovany <syovany@gmail.com>
2020-10-29 02:01:06 -0600

    Se resolvio conflicto que detecto prueba unitaria anteriormente de porcentajeCredito

--- backend/controller/creditos.controller.js
+++ backend/controller/creditos.controller.js
@@ -3,20 +3,25 @@ const mysqldb = require('../database');
 
 usuarioCtrl.getUserForcarnet=async function(req,res,next){
     const { carnet } = req.params;
+    let parametroValido=isNaN(carnet);
+    if(parametroValido){
+        res.json({"estado":"Paramtros no validos"})
+    }else{
+        const sql = `select round((sum(curso.creditos)/250)*100,2) as Porcentaje from usuario
+        join usuario_curso on usuario_curso.ref_carnet = usuario.carnet 
+        join curso on curso.codigo_curso = usuario_curso.ref_codigo_curso
+        join carrera on carrera.codigo_carrera = usuario.ref_codigo_carrera 
+        where usuario.carnet = ${carnet} and usuario_curso.estado = 1;`;
+        mysqldb.connection.query(sql, (error, result) => {
+            if(error) throw error;
+            if(result.length > 0){
+                res.json(result);
+            } else {
+                res.send('No hay resultados');
+            }
+        });
+    }
     //const sql = `select * from usuario where carnet = ${carnet}`;
-    const sql = `select round((sum(curso.creditos)/250)*100,2) as Porcentaje from usuario
-    join usuario_curso on usuario_curso.ref_carnet = usuario.carnet 
-    join curso on curso.codigo_curso = usuario_curso.ref_codigo_curso
-    join carrera on carrera.codigo_carrera = usuario.ref_codigo_carrera 
-    where usuario.carnet = ${carnet} and usuario_curso.estado = 1;`;
-    mysqldb.connection.query(sql, (error, result) => {
-        if(error) throw error;
-        if(result.length > 0){
-            res.json(result);
-        } else {
-            res.send('No hay resultados');
-        }
-    });
 }
 
 usuarioCtrl.getUserForcarnetSuma=async function(req,res,next){
